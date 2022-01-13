007f422d7660104e7bb71803cdff7f0946e66d75
JhovanDaniel <jd069@hotmail.com>
2022-01-13 21:14:58 +0000

    Added admin panel interface and admin navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :appointments
   resources :appointment_tests
   get "appointments", :to => 'appointments#index'
+  get "admin-panel", :to => 'pages#admin_index'
   devise_for :users
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
