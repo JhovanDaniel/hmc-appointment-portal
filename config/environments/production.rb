90b80cab8c0c4b27a835722ac73bce0a95646956
JhovanDaniel <jd069@hotmail.com>
2022-01-19 00:41:58 +0000

    Added production mail configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,27 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  
+  
+config.action_mailer.default_url_options = {host: "hmc-appointment-portal.herokuapp.com", protocol: "https" }  
+#config.action_mailer.delivery_method = :letter_opener
+config.action_mailer.perform_deliveries = true
+
+#~~~~~~~~~~~~~~GMAIL CONFIG ~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               '3b807202c7554c298ebd3679cf736a2f.vfs.cloud9.us-east-2.amazonaws.com',
+  user_name:            'hmc.testmailer@gmail.com',
+  password:             'idanlalihzivkhaw',
+  authentication:       'plain',
+  enable_starttls_auto: true,
+  open_timeout:         5,
+  read_timeout:         5 }
+  
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -66,7 +87,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
